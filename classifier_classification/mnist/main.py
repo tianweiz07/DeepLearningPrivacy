37f246c9e935f70108c56c0909dcaa9707dfe9a7
Ubuntu <ubuntu@ip-172-31-46-56.ec2.internal>
2018-05-17 04:31:13 +0000

    Save model

--- classifier_classification/mnist/main.py
+++ classifier_classification/mnist/main.py
@@ -6,27 +6,64 @@ import argparse
 import os
 import sys
 import numpy as np
+
 import train
+import dataset
+
 
 data1_index = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 data2_index = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+DATA1_DIR = "./data/mnist/"
+DATA2_DIR = "./data/emnist/"
+NUM_TRAIN = 45000
+NUM_TEST = 8000
+
 
 num_sample = 100
 
-param_list = []
-for i in range(num_sample):
-    print("data1: " + str(i))
-    logit_weight = train.Train(data1_index)
-    param_list.append(logit_weight)
-params = np.stack(param_list)
-np.save('param1.npy', params)
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', type=str, default='generate',
+                    help='Either `generate`, or `classify`,')
+
+def generate():
+    data1 = dataset.read_data_sets(DATA1_DIR, DATA2_DIR, reshape=False, one_hot=True,
+                                   num_train=NUM_TRAIN, num_test=NUM_TEST, data_index = data1_index)
+    data2 = dataset.read_data_sets(DATA1_DIR, DATA2_DIR, reshape=False, one_hot=True,
+                                   num_train=NUM_TRAIN, num_test=NUM_TEST, data_index = data2_index)
+
+    for i in range(num_sample):
+        print("---------- Iteration " + str(i) + " ----------")
+        train.Train("model1/" + str(i), data1)
+        train.Train("model2/" + str(i), data2)
+
+
+def main(argv=None):
+
+    FLAGS = parser.parse_args()
+    if (FLAGS.mode == "generate"):
+        generate()
+    elif (FLAGS.mode == "classify"):
+        classify()
+    else:
+        raise ValueError("set --mode as 'generate' or 'classify'")
+
+
+if __name__ == "__main__":
+    main()
+
+#param_list = []
+#for i in range(num_sample):
+#    print("data1: " + str(i))
+#    logit_weight = train.Train(data1_index)
+#    param_list.append(logit_weight)
+#params = np.stack(param_list)
+#np.save('param1.npy', params)
 #x = np.load('result.npy')
 
 
-param_list = []
-for i in range(num_sample):
-    print("data2: " + str(i))
-    logit_weight = train.Train(data2_index)
-    param_list.append(logit_weight)
-params = np.stack(param_list)
-np.save('param2.npy', params)
+#param_list = []
+##    print("data2: " + str(i))
+#    logit_weight = train.Train(data2_index)
+#    param_list.append(logit_weight)
+#params = np.stack(param_list)
+#np.save('param2.npy', params)
--- classifier_classification/mnist/train.py
+++ classifier_classification/mnist/train.py
@@ -9,31 +9,23 @@ import sys
 import numpy as np
 
 import network
-import dataset
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-MODEL_DIR = "./checkpoints/"
-DATA1_DIR = "./data/mnist/"
-DATA2_DIR = "./data/emnist/"
 NUM_EPOCHS = 500
 LOG_EPOCHS = 100
 BATCH_SIZE = 256
 LEARNING_RATE = 1e-3
 NUM_CLASS = 10
 
-NUM_TRAIN = 45000
-NUM_TEST = 8000
 
-data_index = [1, 1, 2, 2, 1, 1, 2, 2, 1, 2]
-
-def Train(data_index = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]):
+def Train(MODEL_DIR, data):
+    tf.reset_default_graph()
     x = tf.placeholder(tf.float32, [None, 28, 28, 1], name="X")
     y = tf.placeholder(tf.int32, [None, NUM_CLASS], name="Y")
     keep_rate = tf.placeholder(tf.float32, name="keep_rate")
 
-    with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
-        logits = network.CreateNet(x, keep_rate)
+    logits = network.CreateNet(x, keep_rate)
 
     soft = tf.nn.softmax(logits, name="soft")
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
@@ -43,9 +35,6 @@ def Train(data_index = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]):
     correct_pred = tf.equal(tf.argmax(soft, 1), tf.argmax(y, 1), name="correct_pred")
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name="accuracy")
 
-    mnist = dataset.read_data_sets(DATA1_DIR, DATA2_DIR, reshape=False, one_hot=True,
-                                   num_train=NUM_TRAIN, num_test=NUM_TEST, data_index = data_index)
-
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
@@ -54,13 +43,13 @@ def Train(data_index = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]):
         step = global_step + 1
 
         while step < global_step + NUM_EPOCHS:
-            batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
+            batch_x, batch_y = data.train.next_batch(BATCH_SIZE)
             feed_dict = {x: batch_x, y: batch_y, keep_rate: 0.8}
             cost_train, acc_train, _ = sess.run([cost, accuracy, optimizer], feed_dict=feed_dict)
 
             if step % LOG_EPOCHS == 0:
-                images_val = mnist.test.images
-                labels_val = mnist.test.labels
+                images_val = data.test.images
+                labels_val = data.test.labels
                 feed_dict = {x: images_val, y: labels_val, keep_rate: 1.0}
                 _, acc_val = sess.run([cost, accuracy], feed_dict=feed_dict)
 
@@ -69,7 +58,10 @@ def Train(data_index = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]):
 
             step += 1
 
-        var = [v for v in tf.trainable_variables() if v.name == "logits/kernel:0"][0]
-        result = sess.run(var)
+        if not os.path.exists(MODEL_DIR):
+            os.makedirs(MODEL_DIR)
+        saver.save(sess, MODEL_DIR+"/model.ckpt")
+#        var = [v for v in tf.trainable_variables() if v.name == "logits/kernel:0"][0]
+#        result = sess.run(var)
 
-        return result
+#        return result
